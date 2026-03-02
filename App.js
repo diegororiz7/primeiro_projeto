aaf14b50025f454ced6d6063ebc9fb41678026f3
diegororiz7 <diegororiz7@gmail.com>
2026-03-02 20:16:33 -0300

    Primeiro commit 02-03-2026

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableHighlight, View } from 'react-native';
+import { Button } from 'react-native-web';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style = {styles.texto}>Aula I</Text>
+      <Text style = {styles.texto2}>React Native 02/03/2026</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +20,14 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  texto:{
+    color: '#FF0000',
+    fontWeight: 'bold',
+    fontSize: 30
+  },
+  texto2:{
+    color: '#00000',
+    fontSize: 20,
+    fontStyle: 'italic'
+  }
 });
