e21034b9aa4e6b0bf61902e8939ff109534ea40a
diegororiz7 <diegororiz7@gmail.com>
2026-03-25 21:52:41 -0300

    Aula 26/03/2026 2

--- components/LanchoneteApp.js
+++ components/LanchoneteApp.js
@@ -4,27 +4,16 @@ import {Text,View,StatusBar,SafeAreaView,SectionList,Button,RefreshControl,TextI
 import styles from '../styles/stylesLanchonete';
 
 const initialMenu = [
-  { title: 'Principal', data: [{ nome: 'Pizza', preco: 30 }, { nome: 'Burger', preco: 25 }, { nome: 'Churrasco', preco: 40 }] },
-  { title: 'Acompanhamento', data: [{ nome: 'Batata frita', preco: 15 }, { nome: 'Anel cebola', preco: 18 }, { nome: 'Dadinho tapioca', preco: 20 }] },
-  { title: 'Bebidas', data: [{ nome: 'Água', preco: 5 }, { nome: 'Refri', preco: 8 }, { nome: 'Cerveja', preco: 12 }] },
-  { title: 'Sobremesa', data: [{ nome: 'Cheese Cake', preco: 22 }, { nome: 'Sorvete', preco: 10 }] },
+  { title: 'Principal', data: ['Pizza', 'Burger', 'Churrasco'] },
+  { title: 'Acompanhamento', data: ['Batata frita', 'Anel cebola', 'Dadinho tapioca'] },
+  { title: 'Bebidas', data: ['Água', 'Refri', 'Cerveja'] },
+  { title: 'Sobremesa', data: ['Cheese Cake', 'Sorvete'] },
 ];
 
 export default function LanchoneteApp() {
   const [menu, setMenu] = useState(initialMenu);
   const [recarregando, setrecarregando] = useState(false);
-  const [novosItens, setNovosItens] = useState({});
-  const [novosPrecos, setNovosPrecos] = useState({});
-  const [selecionados, setSelecionados] = useState([]);
-
-  const recarregar = useCallback(() => {
-    setrecarregando(true);
-    setTimeout(() => {
-      setMenu([...initialMenu]);
-      setSelecionados([]);
-      setrecarregando(false);
-    }, 1000);
-  }, []);
+  const [novoItem, setnovoItem] = useState('');
 
   return (
     <SafeAreaView style={styles.container}>
